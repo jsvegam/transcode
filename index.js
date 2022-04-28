84ba6b92afa308870fb421403ccf1648b840fdda
jsvegam <jsvegam@gmail.com>
2022-04-28 11:42:31 -0400

    Initial commit with change in the comment line

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    // TODO implemet change
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from lambda'),
+    };
+    return response;
+};
+
