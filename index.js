9b7eb05399ee55ada5a9719066d433769e2522e8
jsvegam <jsvegam@gmail.com>
2022-05-27 16:33:16 -0400

    test parameters

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const AWS = require('aws-sdk');
+const url = require('url');
+
 exports.handler = async (event) => {
     // TODO implemet change
+    let varEvent = event.queryStringParameters.code
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from lambda'),
