9193366af7d9cc5fe805298c0db62b452f7773be
jsvegam <jsvegam@gmail.com>
2022-05-29 13:06:46 -0400

    teste secret manager axios

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-const AWS = require('aws-sdk');
 const url = require('url');
+const axios = require('axios');
+const Name = "test_desa_secrets";
+var AWS = require('aws-sdk'),
+    region = "us-east-1",
+    secretName = "test/clave-unica",
+    secret,
+    decodedBinarySecret;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        if (err.code === 'DecryptionFailureException')
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InternalServiceErrorException')
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidParameterException')
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidRequestException')
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'ResourceNotFoundException')
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+    }
+    else {
+        // Decrypts secret using the associated KMS key.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
+        }
+    }
+    
+    // Your code goes here. 
+});
+
+
 
 exports.handler = async (event, context) => {
     // TODO implemet change
@@ -7,8 +57,59 @@ exports.handler = async (event, context) => {
     console.log(event);
     try{
         body = event;
-        //body = event["ClientSecret"];
         console.log(body);
+
+        //consume AWS Secret
+        async function getSecret (secretName) {        
+            const params = {SecretId: secretName}
+            return await new Promise((resolve, reject) => {
+                sm.getSecretValue(params, (err, data) => {
+                    if(err)
+                        reject(err)
+                    else
+                        resolve(data)
+                })
+            })
+        }
+
+        async function getString(secretName) {
+            const secrets = await getSecret(secretName)
+            return secrets.SecretString;
+        }
+
+
+    //consume CU br
+    const usuario = await getString(Name)
+    const { API_Client_Id, API_Client_Secret } = JSON.parse(usuario);    
+
+    const data = {
+        'client_id'     : API_Client_Id,
+        'client_secret' : API_Client_Secret,
+        'grant_type'    : process.env.GRAND_TYPE,
+        'scope'         : process.env.SCOPE,
+        'username'      : process.env.USER,
+        'password'      : process.env.PASS,
+    }  
+
+    axios({
+        method : 'post',
+        url    : api_url, 
+        headers: headers,
+        data   : qs.stringify(data)
+    })
+    .then(function (response) {
+        if (response.status  == '200')
+            console.log("Response : ", response.data);
+    })
+    .catch(function (error) {
+        console.log("Error Code    : ", );
+        console.log("Error Message : ", error.message);
+    })
+
+
+
+
+
     } catch(e){
         body = null;
     }
